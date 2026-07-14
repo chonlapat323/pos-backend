ceb6dfecf62e4a5d8b2eb208d489009447aea9a7
ethanhunt7089 <ethanhunt7089@gmail.com>
2026-07-14 10:39:52 +0700

    Scaffold NestJS backend with Prisma + PostgreSQL

---
+++ src/modules/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {
+    const connectionString = configService.getOrThrow<string>('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
